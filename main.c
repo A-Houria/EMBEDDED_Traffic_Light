7330c7376cc1e93d4e53b920a0d4ac8cc42b9b17
abdelrahman-houria <amr.houria70@gmail.com>
2025-03-30 17:06:25 +0200

    first commit

--- main.c
+++ main.c
@@ -33,29 +33,29 @@ void main(void)
     PM5CTL0 &= ~(1 << 0);
 
     // PORT SETUP
-    P1_DIR |= (1 << 5);     // set P1.5 as output
-    P1_OUT |= (1 << 5);    // set P1.5 output as low (turn off LED)
+    P1_DIR |= (1 << 5);         // set P1.5 as output
+    P1_OUT |= (1 << 5);         // set P1.5 output as low (turn off LED)
 
-    P1_DIR |= (1 << 6);     // set P1.6 as output
-    P1_OUT &= ~(1 << 6);    // set P1.6 output as low (turn off LED)
+    P1_DIR |= (1 << 6);         // set P1.6 as output
+    P1_OUT &= ~(1 << 6);        // set P1.6 output as low (turn off LED)
 
-    P1_DIR |= (1 << 7);     // set P1.7 as output
-    P1_OUT &= ~(1 << 7);    // set P1.7 output as low (turn off LED)
+    P1_DIR |= (1 << 7);         // set P1.7 as output
+    P1_OUT &= ~(1 << 7);        // set P1.7 output as low (turn off LED)
 
     // TIMER SETUP
-    TB0CTL &= ~0x003F;         // Clear previous settings of TB0CTL
-    TB0CTL |= (0b01 << 8);     // Choose ACLK as clock source (32768 Hz)
-    TB0CTL |= (0b01 << 4);     // Choose up mode for the timer
-    TB0CTL |= (0b11 << 6);     // Choose /4 divider for ACLK
-    TB0CCR0 = 40960;           // Set CCR0 value for ~6 seconds (32768 Hz)
-    TB0CCR1 = 12288;           // Set CCR1 value for ~5 seconds
+    TB0CTL &= ~0x003F;          // Clear previous settings of TB0CTL
+    TB0CTL |= (0b01 << 8);      // Choose ACLK as clock source (32768 Hz)
+    TB0CTL |= (0b01 << 4);      // Choose up mode for the timer
+    TB0CTL |= (0b11 << 6);      // Choose /4 divider for ACLK
+    TB0CCR0 = 40960;            // Set CCR0 value for ~10 seconds (32768 Hz)
+    TB0CCR1 = 12288;            // Set CCR1 value for ~3 seconds
 
     // IRQ SETUP FOR TIMER COMPARE
-    TB0CCTL0 |= (1 << 4);      // enable interrupt for CCR0
-    TB0CCTL1 |= (1 << 4);      // enable interrupt for CCR1
-    __enable_interrupt();      // enable maskable interrupts (GIE)
-    TB0CCTL0 &= ~(1 << 0);     // clear flag for CCR0
-    TB0CCTL1 &= ~(1 << 0);     // clear flag for CCR1
+    TB0CCTL0 |= (1 << 4);       // enable interrupt for CCR0
+    TB0CCTL1 |= (1 << 4);       // enable interrupt for CCR1
+    __enable_interrupt();       // enable maskable interrupts (GIE)
+    TB0CCTL0 &= ~(1 << 0);      // clear flag for CCR0
+    TB0CCTL1 &= ~(1 << 0);      // clear flag for CCR1
 
     // MAIN LOOP
     while(1) {}
@@ -66,14 +66,14 @@ void main(void)
 #pragma vector = TIMER0_B0_VECTOR
 __interrupt void ISR_TB0_CCR0() {
     if(count % 2 == 0){
-        P1_OUT &= ~(1 << 5);         // turn off LED on P1.5
-        P1_OUT |= (1 << 6);          // turn on LED on P1.6
-        TB0CCTL0 &= ~(1 << 0);      // clear flag for CCR0
+        P1_OUT &= ~(1 << 5);    // turn off LED on P1.5
+        P1_OUT |= (1 << 6);     // turn on LED on P1.6
+        TB0CCTL0 &= ~(1 << 0);  // clear flag for CCR0
         count++;
     }else{
-        P1_OUT &= ~(1 << 7);         // turn off LED on P1.6
-        P1_OUT |= (1 << 6);         // turn on LED on P1.7
-        TB0CCTL0 &= ~(1 << 0);      // clear flag for CCR0
+        P1_OUT &= ~(1 << 7);    // turn off LED on P1.6
+        P1_OUT |= (1 << 6);     // turn on LED on P1.7
+        TB0CCTL0 &= ~(1 << 0);  // clear flag for CCR0
         count++;
     }
 }
@@ -81,12 +81,12 @@ __interrupt void ISR_TB0_CCR0() {
 #pragma vector = TIMER0_B1_VECTOR
 __interrupt void ISR_TB0_CCR1() {
     if(count % 2 == 0){
-        P1_OUT |= (1 << 5);        // turn on LED on P1.5
-        P1_OUT &= ~(1 << 6);       // turn off LED on P1.7
-        TB0CCTL1 &= ~(1 << 0);     // clear flag for CCR1
+        P1_OUT |= (1 << 5);     // turn on LED on P1.5
+        P1_OUT &= ~(1 << 6);    // turn off LED on P1.7
+        TB0CCTL1 &= ~(1 << 0);  // clear flag for CCR1
     }else{
-        P1_OUT |= (1 << 7);         // turn on LED on P1.7
-        P1_OUT &= ~(1 << 6);        // turn off LED on P1.5
-        TB0CCTL1 &= ~(1 << 0);     // clear flag for CCR1
+        P1_OUT |= (1 << 7);     // turn on LED on P1.7
+        P1_OUT &= ~(1 << 6);    // turn off LED on P1.5
+        TB0CCTL1 &= ~(1 << 0);  // clear flag for CCR1
     }
 }
